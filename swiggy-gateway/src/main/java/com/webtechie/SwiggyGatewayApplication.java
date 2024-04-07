64c8210f52309935b6f6630d82271c31d40871df
snarayanaswamy_pfpt <reach_suresh@outlook.com>
2024-04-07 23:34:06 +0530

    Gateway

--- swiggy-gateway/src/main/java/com/webtechie/SwiggyGatewayApplication.java
+++ swiggy-gateway/src/main/java/com/webtechie/SwiggyGatewayApplication.java
@@ -2,8 +2,10 @@ package com.webtechie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SwiggyGatewayApplication {
 
 	public static void main(String[] args) {

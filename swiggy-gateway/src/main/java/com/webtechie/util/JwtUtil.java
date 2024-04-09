e6c0b80dfc6ec61c2277bb892a352c11a079942d
snarayanaswamy_pfpt <reach_suresh@outlook.com>
2024-04-10 00:20:19 +0530

    Security by gateway

---
+++ swiggy-gateway/src/main/java/com/webtechie/util/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.webtechie.util;
+
+import java.security.Key;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+	public static final String SECRET = "8Zz5tw0Ionm3XPZZfN0NOml3z9FMfmpgXwovR9fp6ryDIoGRM8EPHAB6iHsc0fb";
+	
+	public void validateToken(final String token) {
+		Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+	}
+	
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+}
